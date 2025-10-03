a98b702db5d42278886081df6679fc7f3a7ce6aa
RudeCube <74896419+RudeCube@users.noreply.github.com>
2025-10-03 10:57:51 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-be#!/usr/bin/env sh
+#!/usr/bin/env sh
 # Copyright (c) 2024 Fluent Networks Pty Ltd & AUTHORS All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
